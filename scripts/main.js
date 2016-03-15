69d06ba71112d5ffcb1dea853ed984e1a578fd0f
gregoirepuget <gregoirepuget@gmail.com>
2016-03-15 19:04:57 +0100

    final

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
   console.log(citations.length);
   
+  
+  var historiqueCitations=localStorage.getItem("historiqueCitationsString");
+  if(historiqueCitations == null)
+  {
+    historiqueCitations=new Array();
+  }
+  else
+  {
+    historiqueCitations=JSON.parse(historiqueCitations);
+  }
+  
+  
+  
+  
   $("#numberField").attr("max", citations.length-1);
   
   $("#fortune_form").on("submit", function(event){
@@ -12,13 +26,19 @@ $(document).ready(function(){
   {
     listOptions += '<option value="'+i+'">'+citations[i]["auteur"]+'</option>';
   }
-  console.log(listOptions);
+  //console.log(listOptions);
   $("#authorField").html(listOptions);
   
   
   $("#numberField, #authorField").on("change",function(){
     var nombre = $(this).val();
     
+    
+    historiqueCitations.push(nombre);
+    historiqueCitationsString=JSON.stringify(historiqueCitations);
+    localStorage.setItem("historiqueCitationsString",historiqueCitationsString );
+    
+    console.log(historiqueCitations);
     $("#fortune_cookie").empty();
     $("#fortune_cookie").append("<p>"+citations[nombre]['citation']+"</p>");
     $("#fortune_cookie").append("<span>"+citations[nombre]['auteur']+"</span>");
@@ -28,6 +48,11 @@ $(document).ready(function(){
   $("#fortune_button").on("click", function(event){
     event.preventDefault();
     var nombre=getTirage(citations.length);
+    
+    historiqueCitations.push(nombre);
+    historiqueCitationsString=JSON.stringify(historiqueCitations);
+    localStorage.setItem("historiqueCitationsString",historiqueCitationsString );
+    
     
     $("#fortune_cookie").empty();
     $("#fortune_cookie").append("<p>"+citations[nombre]['citation']+"</p>");
@@ -39,4 +64,49 @@ $(document).ready(function(){
   {
    return Math.floor(Math.random()*limit);
   }
+  
+  $("#nom p").on('blur', function(){
+    localStorage.setItem("nomUser", $(this).html() );
+  });
+  
+  
+  
+  var nomUser = localStorage.getItem("nomUser");
+  
+  if(nomUser != null)
+  {
+    $("#nom p").html(nomUser);
+  }
+  
+  
+  $("#nom span").on('click', function(){
+    localStorage.removeItem("nomUser");
+    $("#nom p").html("Votre nom");
+  });
+  
+  $("#menu button").on("click", function(){
+  
+    $("#menuContent").toggleClass("open");
+    $(this).toggleClass("is-active");
+    
+  
+  });
+  
+  /*citationsString=JSON.stringify(citations);
+  localStorage.setItem("citationDsLocal", citationsString );
+  
+  citations=localStorage.getItem("citationDsLocal");
+  citations=JSON.parse(citations);*/
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 });
